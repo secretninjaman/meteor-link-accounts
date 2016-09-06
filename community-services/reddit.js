00b8a9d3d196a0b61481213621f3649e27ddcd4b
Ayato French <ayato@brachyon.com>
2016-09-05 17:57:07 -0700

    changed package for reddit

--- community-services/reddit.js
+++ community-services/reddit.js
@@ -3,7 +3,7 @@ if(Meteor.isClient){
     if(!Meteor.userId()){
       throw new Meteor.Error(402, "You must be logged in to link an account.");
     }
-    if(!Package["kidovate:reddit"]){
+    if(!Package["pandawhisperer:accounts-reddit"]){
       throw new Meteor.Error(403, "Required package for discord OAuth is missing. Install vincentp:accounts-discord on server.");
     }
     if (! callback && typeof options === "function") {
@@ -11,6 +11,6 @@ if(Meteor.isClient){
       options = null;
     }
     var credentialRequestCompleteCallback = Accounts.oauth.linkCredentialRequestCompleteHandler(callback);
-    Package["kidovate:reddit"].Reddit.requestCredential(options, credentialRequestCompleteCallback);
+    Package["pandawhisperer:accounts-reddit"].RedditOauth.requestCredential(options, credentialRequestCompleteCallback);
   }
 }
